9fd6047c00c11ba5422ce3e2608e1f32f34881c2
AlphaBeast97 <ksaad5272@gmail.com>
2026-06-01 21:05:42 +0500

    refactor: Remove unused email validation regex and password length constants from utils.dart

--- frontend/techfix/lib/shared/utils.dart
+++ frontend/techfix/lib/shared/utils.dart
@@ -3,12 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:techfix/screens/login_screen.dart';
 import 'package:techfix/state/app_session_scope.dart';
 
-/// Regex for basic email format validation.
-const emailRegex = r'^[^@\s]+@[^@\s]+\.[^@\s]+$';
-
-/// Minimum password length enforced on sign-up.
-const minPasswordLength = 6;
-
 /// Clears the session and navigates back to [LoginScreen],
 /// removing all previous routes from the stack.
 void signOut(BuildContext context) {
