d8c54ecc3950f717bba3a188f8d62b1865044b70
AlphaBeast97 <ksaad5272@gmail.com>
2026-06-26 16:15:14 +0500

    feat: initialize front-end project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created favicon.svg for the project.
    - Implemented main application component (App.jsx) with a simple greeting.
    - Added global styles using Tailwind CSS in index.css.
    - Set up entry point (main.jsx) for rendering the application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ frontend/TechFix by Vite/adv-front-end-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>hello sir</div>
+    </>
+  );
+}
+
+export default App;
